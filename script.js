77c938555628cd33e1bbc3e45d0f9d9200c14787
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-08-22 19:15:50 +0600

    added select part

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+// function getButtonValue(Id){
+//     const tamim =document.getElementById(Id);
+//     const tamimParent = tamim.parentNode.firstElementChild;
+//     const getSelectName = tamimParent.innerText;
+//     console.log(getSelectName);
+// }
+// getButtonValue('tamim-button');
+
+
+// function getPlayerName(){
+
+// }
+
+
+
+for(let i = 0; i<10;i++){
+    
+    const button = document.querySelectorAll('.select-button')[i];
+
+        button.addEventListener('click',function(){
+            const parent = button.parentNode;
+            const firstChild = parent.firstElementChild;
+            const name = firstChild.innerHTML;
+           
+            // put selected name and set the order list 
+
+            const orderList = document.querySelector('.order-list')
+            const listItem = document.createElement('li')
+
+            listItem.innerText=name;
+            orderList.appendChild(listItem);
+        
+        })    
+    };
+
+
+
